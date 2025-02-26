ed587e4617a9e5c9f41efa45a083306c9ab3cfb5
Davimteixeira <daviteixeira077@gmail.com>
2025-02-26 10:00:44 -0300

    colocando a api no codigo

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'http://154.53.37.30:86/api',
   headers: {
     'Content-Type': 'application/json',
   },
